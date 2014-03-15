90dcc479d73529718a70c72f88ce838ae6872fe4
steveprim <smp7d@yahoo.com>
2014-03-15 14:14:55 -0400

    more tests for building plateau

--- src/test/java/org/sample/rover/simulator/MarsRoverSimulatorTest.java
+++ src/test/java/org/sample/rover/simulator/MarsRoverSimulatorTest.java
@@ -5,6 +5,7 @@ import junit.framework.Assert;
 import org.junit.Test;
 import org.sample.rover.entity.Plateau;
 import org.sample.rover.entity.StatelessRectangularPlateau;
+import org.sample.rover.exception.InvalidPlateauDirective;
 
 /**
  * Unit test for mars simulator.
@@ -13,8 +14,27 @@ import org.sample.rover.entity.StatelessRectangularPlateau;
 public class MarsRoverSimulatorTest {
 	@Test
 	public void testBuildPlateau() {
+		buildPlateauHelper("5 4");
+	}
+
+	@Test(expected = InvalidPlateauDirective.class)
+	public void testBuildPlateau_invalidWidth() {
+		buildPlateauHelper("x 4");
+	}
+
+	@Test(expected = InvalidPlateauDirective.class)
+	public void testBuildPlateau_invalidHeight() {
+		buildPlateauHelper("5 y");
+	}
+
+	@Test(expected = InvalidPlateauDirective.class)
+	public void testBuildPlateau_invalidFormat() {
+		buildPlateauHelper("blah");
+	}
+
+	private void buildPlateauHelper(String directive) {
 		MarsRoverSimulator simulator = new MarsRoverSimulator();
-		Plateau plateau = simulator.buildPlateau("5 4");
+		Plateau plateau = simulator.buildPlateau(directive);
 		Assert
 				.assertTrue(
 						"Plateaus on Mars are supposed to be stateless and rectangular!",
